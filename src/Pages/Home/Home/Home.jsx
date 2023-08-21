a900fe55fbad298ca4824c747d6e73762bb1ad8e
hrfahimm <fahimtaj10@gmail.com>
2023-08-21 23:52:42 +0600

    header-footer-hero-carosual

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Categori from '../Categori/Categori';
+
+const Home = () => {
+   return (
+      <div>
+         <Banner />
+         <Categori />
+      </div>
+   );
+};
+
+export default Home;
